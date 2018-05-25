b8a2a73955f2f37ef05d471403468a77acfb5b65
Ubuntu <ubuntu@ip-172-31-12-18.ap-northeast-1.compute.internal>
2018-05-25 12:10:16 +0000

    noisy

--- DQN_pinball/DQN.py
+++ DQN_pinball/DQN.py
@@ -21,7 +21,9 @@ from skimage.viewer import ImageViewer
 import tensorflow as tf
 import pylab
 import argparse
+from NoisyDense import NoisyDense
 
+SIGMA_INIT = 0.02
 EPISODES = 30000
 img_rows , img_cols = 80, 80
 #Convert image into Black and white
@@ -29,7 +31,7 @@ img_channels = 4 #We stack 4 frames
 
 class DQNAgent:
     def __init__(self, state_size, action_size, args):
-	self.args = args
+        self.args = args
         self.t = 0
         self.max_Q = 0
         self.train = True
@@ -41,10 +43,11 @@ class DQNAgent:
         # these is hyper parameters for the DQN
         self.discount_factor = 0.99
         self.learning_rate = 1e-4
-        if(self.train):
+        if(self.train and self.args['noisy']=="False"):
             self.epsilon = 1.0
         else:
-            self.epsilon = 1e-6
+            print("For Noisy Net we set to 1e-6")
+            self.epsilon = 1e-6     #For NoisyNet we don't need explore
         self.epsilon_decay = 0.999
         self.epsilon_min = 1e-6
         self.batch_size = 32
@@ -67,13 +70,22 @@ class DQNAgent:
         conv2 = Convolution2D(64, 4, 4, subsample=(2, 2), activation='relu')(conv1)
         conv3 = Convolution2D(64, 3, 3, subsample=(1, 1), activation='relu')(conv2)
         flatten = Flatten()(conv3)        
-        if (self.args['dueling'] == "True"):    #If we use dueling
-            print("Now we use dueling network")    
+        if (self.args['dueling'] == "True" and self.args['noisy'] == "False"):    #If we use dueling
+            print("Now we use vanilla dueling network")    
             fc1 = Dense(512)(flatten)
             advantage = Dense(self.action_size)(fc1)
             fc2 = Dense(512)(flatten)
             value = Dense(1)(fc2)
             policy = merge([advantage, value], mode = lambda x: x[0]-K.mean(x[0])+x[1], output_shape = (self.action_size,))
+        elif (self.args['dueling'] == "True" and self.args['noisy'] == "True"):
+            fc1 = Dense(512)(flatten)
+            advantage = NoisyDense(self.action_size,name='advantage',sigma_init=SIGMA_INIT)(fc1)
+            fc2 = Dense(512)(flatten)
+            value = NoisyDense(1, name='value',sigma_init=SIGMA_INIT)(fc2)
+            policy = merge([advantage, value], mode = lambda x: x[0]-K.mean(x[0])+x[1], output_shape = (self.action_size,))
+        elif (self.args['dueling'] == "False" and self.args['noisy'] == "True"):
+            fc1 = Dense(512)(flatten)
+            policy = NoisyDense(self.action_size,name='policy',sigma_init=SIGMA_INIT)(fc1)
         else:
             fc1 = Dense(512)(flatten)
             policy = Dense(self.action_size)(fc1)
@@ -141,6 +153,15 @@ class DQNAgent:
             Q_sa = self.target_model.predict(state_t1)
             targets[range(batch_size), action_t] = reward_t + self.discount_factor*np.max(Q_sa, axis=1)*np.invert(terminal)
 
+        #Now we sample noisy
+        if (self.args['dueling'] == "False" and self.args['noisy'] == "True"):
+            self.model.get_layer('policy').sample_noise()
+        elif (self.args['dueling'] == "True" and self.args['noisy'] == "True"):
+            self.model.get_layer('advantage').sample_noise()
+            self.model.get_layer('value').sample_noise()
+        else:
+            self.test = 0  #Do nothing
+
         self.model.train_on_batch(state_t, targets)
 
     def load_model(self, name):
@@ -162,6 +183,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Description of your program')
     parser.add_argument('--doubleQ', help='Use Double DQN or not', required=True)
     parser.add_argument('--dueling', help='Use Dueling or not', required=True)
+    parser.add_argument('--noisy', help='Use NoisyNet or not', required=True)
     parser.add_argument('--name', help='FileName',required=True)
     args = vars(parser.parse_args())
 
